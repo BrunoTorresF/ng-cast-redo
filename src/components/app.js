b03ac67b9d1ac08c2a946f6c8c7dc61bf73a63c8
BrunoTorresF <bruno.torres.fabila@gmail.com>
2018-05-24 18:55:17 -0500

    Edit app

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,13 @@
 angular.module('video-player', [])
 
 .component('app', {
-  templateUrl: 'src/templates/app.html'
+  templateUrl: 'src/templates/app.html',
+})
+
+.controller('AppCtrl', function() {
+  this.videos = exampleVideoData;
+  this.currentVideo = this.videos[0];
+  this.handleVideoEntryClick = (video) => {
+    this.currentVideo = video;
+  }
 });
